63d887990eeafd676cbb092ec49c8fa0edb2c803
“msoto5” <miguel@soto.es>
2024-02-07 15:18:17 +0000

    Review tests ch5

--- populate_rango.py
+++ populate_rango.py
@@ -46,7 +46,7 @@ def populate():
     
     # Create
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'], p['views'])
 
@@ -61,8 +61,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
